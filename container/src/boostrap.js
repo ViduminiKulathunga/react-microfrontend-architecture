4baaa136014ccbe2a25c63117db27eebe2a94c35
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-23 15:00:53 +0200

    Consume remte modules

--- container/src/boostrap.js
+++ container/src/boostrap.js
@@ -1,4 +1,5 @@
-import "productsApp/ProductsIndex";
-import "cart/CartShow";
+import { mount as productsMount } from "productsApp/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
-
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#cart-dev"));
